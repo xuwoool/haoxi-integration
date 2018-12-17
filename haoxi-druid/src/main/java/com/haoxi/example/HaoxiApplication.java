72dd98bf34d38b4ba5e07df3d8172a5260855892
beckxu <xuwoool@qq.com>
2018-12-17 21:34:17 +0800

    Initial commit

---
+++ haoxi-druid/src/main/java/com/haoxi/example/HaoxiApplication.java
@@ -0,0 +1,14 @@
+package com.haoxi.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaoxiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaoxiApplication.class, args);
+    }
+
+}
+
